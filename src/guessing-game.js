74ab440334f8db78ae399d41c5abc10dcb5f2d9f
meller96 <meller70@gmail.com>
2017-09-29 21:01:35 +0300

    ggame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
-class GuessingGame {
-    constructor() {}
+class GuessingGame { 
+constructor() { 
+this.max = 0; 
+this.min = 0; 
+this._try = 0; 
+} 
 
-    setRange(min, max) {
+setRange(min, max) { 
+if (max < min) 
+return -1; 
+this.min = min; 
+this.max = max; 
+} 
 
-    }
+guess() { 
+this._try = this.min + Math.round((this.max - this.min) / 2); 
+return this._try; 
+} 
 
-    guess() {
+lower() { 
+this.max = this._try; 
+} 
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
+greater() { 
+this.min = this._try; 
+} 
+} 
 
 module.exports = GuessingGame;
